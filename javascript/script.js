10e72af3f0d7df434d6787e61b23a56df150ece5
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2018-06-02 21:41:56 -0300

    Book-Style 1.0
    Foram add todos os arquivos

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+
+   document.getElementById("menubardark").addEventListener("click", function(){
+       document.body.style.background = "#222";
+       document.body.style.color = "#fff";   
+       document.getElementById("menubardark").style.color = "#222";  
+       document.getElementById("menubarlight").style.color = "#222";
+       document.getElementById("menubara1").style.color = "#222";
+       document.getElementById("menubara2").style.color = "#222";
+       document.getElementById("menubara3").style.color = "#222";  
+   });
+
+   document.getElementById("menubarlight").addEventListener("click", function(){
+       document.body.style.background = "#fff";
+       document.body.style.color = "#222";
+   });
+
+   document.getElementById("menubara1").addEventListener("click", function(){
+       document.body.style.fontSize = "16px";
+       document.getElementById("menubardark").style.fontSize = "16px";
+       document.getElementById("menubarlight").style.fontSize = "16px";
+       document.getElementById("menubara1").style.fontSize = "16px";
+       document.getElementById("menubara2").style.fontSize = "16px";
+       document.getElementById("menubara3").style.fontSize = "16px";
+   });
+
+   document.getElementById("menubara2").addEventListener("click", function(){
+       document.body.style.fontSize = "18px";
+       document.getElementById("menubardark").style.fontSize = "16px";
+       document.getElementById("menubarlight").style.fontSize = "16px";
+       document.getElementById("menubara1").style.fontSize = "16px";
+       document.getElementById("menubara2").style.fontSize = "16px";
+       document.getElementById("menubara3").style.fontSize = "16px";
+   });
+
+   document.getElementById("menubara3").addEventListener("click", function(){
+       document.body.style.fontSize = "20px";
+       document.getElementById("menubardark").style.fontSize = "16px";
+       document.getElementById("menubarlight").style.fontSize = "16px";
+       document.getElementById("menubara1").style.fontSize = "16px";
+       document.getElementById("menubara2").style.fontSize = "16px";
+       document.getElementById("menubara3").style.fontSize = "16px";
+   });
